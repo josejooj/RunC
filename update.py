2210e3e93c4b3c16a183c56e1c8cff5f84f65568
cleiton2040 <grimmyshad@gmail.com>
2022-01-12 21:44:16 -0300

    t

--- update.py
+++ update.py
@@ -1,8 +1,16 @@
 import sys
 import subprocess
+import re
+import math
 
-descricao = input('Qual a descição do seu commit?')
-branch = input('Qual a branch?')
+descricao = sys.argv[1]
+
+if re.search(r'^name=[^ \n]+$', sys.argv[1]) == None: descricao = input('Qual a descição do seu commit? ')
+else: descricao = sys.argv[1][5:math.inf]
+
+print(descricao)
+
+branch = input('Qual a branch? ')
 
 subprocess.run('git add .')
 subprocess.run(f'git commit -m "{descricao}"')
